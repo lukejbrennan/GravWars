10f820c98a84656dceedc886c21805c1fd9cfc8e
Luke Brennan <lukejohnbrennan@gmail.com>
2017-05-10 22:43:07 -0400

    nope

--- conn.py
+++ conn.py
@@ -1,16 +1,44 @@
-# Home server to get connections from client and and work
-
+#!/usr/bin/python
 from twisted.internet.protocol import Factory
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
-from twisted.internet.defer import DeferredQueue
 import time
 import connections
-COMMAND_PORT = 40037
-DATA_PORT = 42037
-CLIENT_PORT = 43037
+COMMAND_PORT = 40011
+DATA_PORT = 42011
+CLIENT_PORT = 43011
+
+###########################################
+# P1 Server Connection
+###########################################
+
+class P1Connection(Protocol):
+    def __init__(self, gs):
+        self.gs = gs
+
+    def connnectionMade(self):
+        print "conn made"
+        gs.getConnRef(self)
+        gs.main('p1')
+
+    def dataReceived(self, data):
+        if data == 'spaceship_collision':
+            #end game
+            return
+        elif data == 'planet_collision':
+            self.gs.is_your_turn = not self.gs.is_you_turn
+        else:
+            print "Error: Unknown data"
+
+class p1Factory(Factory):
+    def __init__(self, gs):
+		self.myconn = P1Connection(gs)
+		print "connection factory initialized"
+
+    def buildProtocol(self, addr):
+        return self.myconn
 
 #Listen for command connection
-print 'listening for a command connection on port 40037'
-reactor.listenTCP(COMMAND_PORT, connections.p2Factory(2))
+print 'listening for a command connection on port 40011'
+reactor.listenTCP(40011, p1Factory(2))
 reactor.run()
--- conn2.py
+++ conn2.py
@@ -1,14 +1,42 @@
-# Work client to connect to home.py proxy server
-
+#!/usr/bin/python
 from twisted.internet.protocol import ClientFactory
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
-from twisted.internet.defer import DeferredQueue
 from twisted.python import log
 import connections
-COMMAND_PORT = 40037
-DATA_PORT = 42037
+COMMAND_PORT = 40011
+DATA_PORT = 42011
 
-if __name__ == '__main__':
-    reactor.connectTCP("ash.campus.nd.edu", COMMAND_PORT, connections.p2Factory(2))
-    reactor.run()
+###########################################
+# P2 Client Connection
+###########################################
+
+class P2Connection(Protocol):
+    def __init__(self, gs):
+		self.gs = gs
+		print "connection initialized"
+
+    def connnectionMade(self):
+        print "conn_made"
+        gs.getConnRef(self)
+        gs.main('p2')
+
+    def dataReceived(self, data):
+        if data == 'spaceship_collision':
+            #end game
+            return
+        elif data == 'planet_collision':
+            self.gs.is_your_turn = not self.gs.is_you_turn
+        else:
+            data = data.split('\n')
+            self.gs.planets.append(pickle.loads(data[1]))
+
+class p2Factory(ClientFactory):
+    def __init__(self, gs):
+		self.myconn = P2Connection(gs)
+		print "factory initialized"
+    def buildProtocol(self, addr):
+        return self.myconn
+
+reactor.connectTCP("ash.campus.nd.edu", 40011, p2Factory(2))
+reactor.run()
